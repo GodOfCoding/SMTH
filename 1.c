a7bc24121027e11e6d645b22646f5477740c553f
GodOfCoding <people-faces@yandex.ru>
2014-09-17 21:40:47 +0400

    Update 1.c

--- 1.c
+++ 1.c
@@ -9,62 +9,58 @@ int main(){
   int pipeFd[2];
   pipe(pipeFd);
   childPid = fork();
-  if(childPid == 0){ //child process
+  if(childPid == 0){
+    /* Дочерний процесс */
     close(pipeFd[1]);
     float mas1[2];
-   float x, y;
-   if(read(pipeFd[0], &mas1, sizeof(mas1)) ){ //on
-     x=mas1[0];
-     y=mas1[1];
- printf("\n%f   %f\n", x, y);
-       if(x==.0 && y==.0){
-    printf("Point is at the origin\n");
-    return 0;
-  }
-  if(x==.0){
-    printf("Point is situated at the axis Y\n");
-    return 0;
-  }
-  if(y==.0){
-    printf("Point is situated at the axis X\n");
-    return 0;
-  }
-  if(x>.0){
-    if(y>.0){
-      printf("First quarter\n");
+    float x, y;
+    if(read(pipeFd[0], &mas1, sizeof(mas1)) ){ 
+      x=mas1[0];
+      y=mas1[1];
+      /* Проверка, в какой четверти точка */
+      if(x==.0 && y==.0){
+        printf("Point is at the origin\n");
+        return 0;
+        }
+      if(x==.0){
+        printf("Point is situated at the axis Y\n");
+        return 0;
+      }
+      if(y==.0){
+        printf("Point is situated at the axis X\n");
+        return 0;
+      }
+      if(x>.0){
+        if(y>.0){
+          printf("First quarter\n");
+        }
+        else{
+          printf("Fourth quarter\n");
+        }
+      }
+      if(x<.0){
+        if(y>.0){
+          printf("Second quarter\n");
+        }
+        else{
+          printf("Third quarter\n");
+        }
+      }
     }
-    else{
-       printf("Fourth quarter\n");
-    }
-  }
-  if(x<.0){
-    if(y>.0){
-       printf("Second quarter\n");
-    }
-    else{
-      printf("Third quarter\n");
-    }
-  }
-    }
-    else //fail
+    else 
       close(pipeFd[0]);
-    
-    //		return 0;
   }
   
   else{
-    //ðîäèòåëüñêèé ïðîöåññ 
+    /* родительский процесс */
     close(pipeFd[0]);
-    //float x1, y1;
-  float mas[2];
-  printf("Enter x: ");
-  scanf("%f", &mas[0]);
-  printf("Enter y: ");
-  scanf("%f", &mas[1]);
-  printf("%f   %f\n", mas[0], mas[1]);
-
-    
-  write(pipeFd[1], &mas, sizeof(mas));
-    
+    float mas[2];
+    /* Считываем x и y в массив */
+    printf("Enter x: ");
+    scanf("%f", &mas[0]);
+    printf("Enter y: ");
+    scanf("%f", &mas[1]);
+    /* Пишем в ТРУБУ */
+    write(pipeFd[1], &mas, sizeof(mas));
   }
 }
