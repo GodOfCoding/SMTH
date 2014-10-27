8f0995e155ab9bd5203342b595619dfbb893e3d7
GodOfCoding <people-faces@yandex.ru>
2014-10-27 21:09:57 +0400

    comp

--- 1.c
+++ 1.c
@@ -1,66 +1,52 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/wait.h>
-
-int main(){
-  pid_t childPid;
-  int pipeFd[2];
-  pipe(pipeFd);
-  childPid = fork();
-  if(childPid == 0){
-    /* Дочерний процесс */
-    close(pipeFd[1]);
-    float mas1[2];
-    float x, y;
-    if(read(pipeFd[0], &mas1, sizeof(mas1)) ){ 
-      x=mas1[0];
-      y=mas1[1];
-      /* Проверка, в какой четверти точка */
-      if(x==.0 && y==.0){
-        printf("Point is at the origin\n");
-        return 0;
-        }
-      if(x==.0){
-        printf("Point is situated at the axis Y\n");
-        return 0;
-      }
-      if(y==.0){
-        printf("Point is situated at the axis X\n");
-        return 0;
-      }
-      if(x>.0){
-        if(y>.0){
-          printf("First quarter\n");
-        }
-        else{
-          printf("Fourth quarter\n");
-        }
-      }
-      if(x<.0){
-        if(y>.0){
-          printf("Second quarter\n");
-        }
-        else{
-          printf("Third quarter\n");
-        }
-      }
-    }
-    else 
-      close(pipeFd[0]);
-  }
-  
-  else{
-    /* родительский процесс */
-    close(pipeFd[0]);
-    float mas[2];
-    /* Считываем x и y в массив */
-    printf("Enter x: ");
-    scanf("%f", &mas[0]);
-    printf("Enter y: ");
-    scanf("%f", &mas[1]);
-    /* Пишем в ТРУБУ */
-    write(pipeFd[1], &mas, sizeof(mas));
-  }
-}
+﻿bool operator < (const VectorElem &elem) const{
+		int status1;
+		int status2;
+		int status3;
+		for(int i = 6; i >= 0;  i--){
+			if(num3[i] - '0' < elem.num3[i] - '0'){
+				status3 = 1;
+			}
+			else if(num3[i] - '0' == elem.num3[i] - '0'){
+				status3 = 3;
+			}
+			else {
+				status3 = 0;
+			}
+		}
+		for(int i = 2; i >= 0;  i--){
+			if(num2[i] - '0' < elem.num2[i] - '0'){
+				status2 = 1;
+			}
+			else if(num2[i] - '0' == elem.num2[i] - '0'){
+				status2 = 3;
+			}
+			else {
+				status2 = 0;
+			}
+		}
+		for(int i = 2; i >= 0;  i--){
+			if(num1[i] - '0' < elem.num1[i] - '0'){
+				status1 = 1;
+			}
+			else if(num1[i] - '0' == elem.num1[i] - '0'){
+				status1 = 3;
+			}
+			else {
+				status1 = 0;
+			}
+		}
+		if(status1 == 1){
+			return true;
+		}
+		if(status1 == 3){
+			if(status2 == 1 ){
+				return true;
+			}
+			if(status2 == 3){
+				if(status3 == 1){
+					return true;
+				}
+			}
+		}
+        return false;
+    };
